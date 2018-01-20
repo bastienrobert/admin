63512b4b875a84313aebd16ce24ccd101b33ebb9
Bastien <bastienrbt@protonmail.com>
2018-01-20 21:50:15 +0100

    Securize the order check

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,14 @@ class DashboardController < ApplicationController
 
   # GET /dashboard
   def index
-    @orders = snipcart_request('orders')['items']
+    unless current_user.admin
+      @orders = snipcart_request('orders')['items'].select { |v|
+        DateTime.parse(v['creationDate']) >= current_user.periods.last.start_date.beginning_of_day &&
+        DateTime.parse(v['creationDate']) <= current_user.periods.last.end_date.end_of_day
+      }
+    else
+      @orders = snipcart_request('orders')['items']
+    end
   end
 
   # GET /order/:id
@@ -14,5 +21,14 @@ class DashboardController < ApplicationController
 
   def set_order
     @order = snipcart_request('orders')['items'].select {|v| v['token'] == params[:id]}[0]
+    unless current_user.admin
+      start = DateTime.parse(@order['creationDate']) >= current_user.periods.last.start_date.beginning_of_day
+      stop = DateTime.parse(@order['creationDate']) <= current_user.periods.last.end_date.end_of_day
+      if start == true && stop == true
+        return @order
+      else
+        redirect_to user_root_url, notice: "Vous n'avez pas accès à cette commande"
+      end
+    end
   end
 end
