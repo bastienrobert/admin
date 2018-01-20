2f5415ab023a405cadc56051e92c050ef08b0708
Bastien <bastienrbt@protonmail.com>
2018-01-20 12:14:17 +0100

    Add user select on period create/edit and auto select user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :periods
+  resources :periods, except: [:new] do
+    collection do
+      get 'new/:user_id', :to => "periods#new", :as => 'new'
+    end
+  end
 
   devise_for :users
   devise_scope :user do
