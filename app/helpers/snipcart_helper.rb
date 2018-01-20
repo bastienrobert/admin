3860b181b3825ad2934d1f7dc60dadc4fab75dbd
Bastien <bastienrbt@protonmail.com>
2018-01-20 12:34:27 +0100

    Snipcart request helper has been improved, now it takes params and method can be choose

--- app/helpers/snipcart_helper.rb
+++ app/helpers/snipcart_helper.rb
@@ -1,17 +1,29 @@
 module SnipcartHelper
-  require 'uri'
+
   require 'net/http'
+
   def snipcart_request(req, params = {}, met = 'GET')
+
+    ## HOW TO USE IT
+    # snipcart_request('URL', PARAMS IN A HASH, METHOD)
+    #
+    # example : snipcart_request('orders/1', {status: 'Cancelled'}, 'PUT')
+    # this will set status Cancelled to order with ID 1 using PUT method
+    #
+    # /!\ If you're using GET method, you can just set your request
+    # example : snipcart_request('orders') will use GET method without params, it'll display all your orders
+
     url = URI("https://app.snipcart.com/api/" + req)
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
 
-    request = Net::HTTP::Get.new(url)
+    request = Net::HTTP.const_get(met.capitalize).new(url)
     request["accept"] = 'application/json'
     request["authorization"] = 'Basic ' + ENV['SNIPCART_API_KEY']
     request["content-type"] = 'application/x-www-form-urlencoded'
     request["cache-control"] = 'no-cache'
+    request.set_form_data(params)
 
     response = http.request(request)
 
